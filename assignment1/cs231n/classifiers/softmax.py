Fixed loss function and gradients of softmax classifier

assignment1/cs231n/classifiers/softmax.py
@@ -36,20 +36,21 @@ def softmax_loss_naive(W, X, y, reg):
   # Softmax Loss
   for i in range(num_train):
     f = scores[i] - np.max(scores[i]) # avoid numerical instability
-    softmax = np.exp(f)/np.sum(np.exp(f)) + 1e-8 # to avoid log(0)
+    softmax = np.exp(f)/np.sum(np.exp(f))
     loss += -np.log(softmax[y[i]])
     # Weight Gradients
     for j in range(num_classes):
       dW[:,j] += X[i] * softmax[j]
     dW[:,y[i]] -= X[i]
 
+  # Average
+  loss /= num_train
+  dW /= num_train
+
   # Regularization
   loss += reg * np.sum(W * W)
   dW += reg * 2 * W 
 
-  # Average
-  loss /= num_train
-  dW /= num_train
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -79,20 +80,21 @@ def softmax_loss_vectorized(W, X, y, reg):
   
   # Softmax Loss
   sum_exp_scores = np.exp(scores).sum(axis=1, keepdims=True)
-  softmax_matrix = np.exp(scores)/sum_exp_scores + 1e-8 # to avoid log(0)
+  softmax_matrix = np.exp(scores)/sum_exp_scores
   loss = np.sum(-np.log(softmax_matrix[np.arange(num_train), y]) )
 
   # Weight Gradient
   softmax_matrix[np.arange(num_train),y] -= 1
   dW = X.T.dot(softmax_matrix)
 
+  # Average
+  loss /= num_train
+  dW /= num_train
+
   # Regularization
   loss += reg * np.sum(W * W)
   dW += reg * 2 * W 
 
-  # Average
-  loss /= num_train
-  dW /= num_train
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################