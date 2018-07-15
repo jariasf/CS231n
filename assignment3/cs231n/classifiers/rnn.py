d9f73cd0ab61321bf8b1a552052ceabf8d0daf11
jariasf <jariasf03@gmail.com>
2018-07-14 22:15:03 -0500

    Implemented CaptioningRNN loss and sample

--- assignment3/cs231n/classifiers/rnn.py
+++ assignment3/cs231n/classifiers/rnn.py
@@ -140,7 +140,21 @@ class CaptioningRNN(object):
         # Note also that you are allowed to make use of functions from layers.py   #
         # in your implementation, if needed.                                       #
         ############################################################################
-        pass
+        # loss
+        h0 = features.dot(W_proj) + b_proj                                 #[NxH]
+        x, cache_we = word_embedding_forward(captions_in, W_embed)         #[NxTxW]
+        h, cache_rnn = rnn_forward(x, h0, Wx, Wh, b)                       #[NxTxH]
+        out, cache_voc = temporal_affine_forward(h, W_vocab, b_vocab)      #[NxTxV]
+        loss, dout = temporal_softmax_loss(out, captions_out, mask, verbose=False)
+
+        # gradients
+        dh, dW_vocab, db_vocab = temporal_affine_backward(dout, cache_voc)
+        dx, dh0, dWx, dWh, db = rnn_backward(dh, cache_rnn)
+        dW_embed = word_embedding_backward(dx, cache_we)
+        dW_proj = features.T.dot(dh0)
+        db_proj = dh0.sum(axis=0)
+        grads = {'W_vocab':dW_vocab, 'b_vocab':db_vocab, 'Wx':dWx, 'Wh':dWh, 
+                 'b':db, 'W_embed':dW_embed, 'W_proj':dW_proj, 'b_proj':db_proj}
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -205,7 +219,16 @@ class CaptioningRNN(object):
         # NOTE: we are still working over minibatches in this function. Also if   #
         # you are using an LSTM, initialize the first cell state to zeros.        #
         ###########################################################################
-        pass
+        h0 = features.dot(W_proj) + b_proj
+        V, W = W_embed.shape
+        x = np.ones((N, W)) * W_embed[self._start]
+        for i in range(max_length):
+            next_h, _ = rnn_step_forward(x, h0, Wx, Wh, b)
+            out = next_h.dot(W_vocab) + b_vocab
+            max_indices = out.argmax(axis=1)
+            captions[:,i] = max_indices
+            x = W_embed[max_indices]
+            h0 = next_h
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
